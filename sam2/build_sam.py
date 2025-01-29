5dee06ba87ba56aae89ed568d68accea71385701
Jannis Becktepe <jannis@becktepe.de>
2025-01-29 12:07:44 +0100

    enabling the use of hydra to run MedSAM2 by disabling hydra inits

--- sam2/build_sam.py
+++ sam2/build_sam.py
@@ -7,32 +7,18 @@
 import logging
 
 import torch
-from hydra import compose
+from hydra.experimental import compose
 from hydra.utils import instantiate
-from omegaconf import OmegaConf
+from omegaconf import OmegaConf, DictConfig
 
 
 def build_sam2(
-    config_file,
+    model_cfg: DictConfig,
     ckpt_path=None,
     device="cuda",
     mode="eval",
-    hydra_overrides_extra=[],
-    apply_postprocessing=True,
 ):
-
-    if apply_postprocessing:
-        hydra_overrides_extra = hydra_overrides_extra.copy()
-        hydra_overrides_extra += [
-            # dynamically fall back to multi-mask if the single mask is not stable
-            "++model.sam_mask_decoder_extra_args.dynamic_multimask_via_stability=true",
-            "++model.sam_mask_decoder_extra_args.dynamic_multimask_stability_delta=0.05",
-            "++model.sam_mask_decoder_extra_args.dynamic_multimask_stability_thresh=0.98",
-        ]
-    # Read config and init model
-    cfg = compose(config_name=config_file, overrides=hydra_overrides_extra)
-    OmegaConf.resolve(cfg)
-    model = instantiate(cfg.model, _recursive_=True)
+    model = instantiate(model_cfg, _recursive_=True)
     _load_checkpoint(model, ckpt_path)
     model = model.to(device)
     if mode == "eval":
